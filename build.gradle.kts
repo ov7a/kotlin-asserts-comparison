29950ce748402dacd0f350b9ba46f4877bacf7e7
ov7a <ov7a@yandex.ru>
2020-07-11 18:11:44 +0300

    minor: unification of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.assertj", "assertj-core", "3.16.1")
-    testImplementation("io.kotest", "kotest-assertions-core-jvm", "4.1.1")
-    testImplementation("io.kotest", "kotest-assertions-core", "4.1.1")
+    testImplementation("org.assertj:assertj-core:3.16.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.1.1")
+    testImplementation("io.kotest:kotest-assertions-core:4.1.1")
     testImplementation("com.google.truth:truth:1.0.1")
     testImplementation("org.hamcrest:hamcrest:2.2")
     testImplementation("io.strikt:strikt-core:0.26.1")
